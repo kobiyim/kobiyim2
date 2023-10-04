b5db05463cbeaa30b7a0a7337f917ac39b17af85
Nuh <79440046+kobiyim@users.noreply.github.com>
2023-10-04 10:47:14 +0300

    controller a derleyeci cevap eklendi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function arrangeErrors($errors)
+    {
+        $data = [];
+
+        foreach ($errors as $key => $errorDetails) {
+            $data[] = [
+                'key'     => $key,
+                'message' => implode(' ', $errorDetails),
+            ];
+        }
+
+        return $data;
+    }
 }
