d1815ebb87cad29829e38a0df8244e05a4a28251
Nuh <79440046+kobiyim@users.noreply.github.com>
2023-10-26 23:41:19 +0300

    logo düzenlemeleri

--- resources/views/logo/aside.blade.php
+++ resources/views/logo/aside.blade.php
@@ -1,5 +1,5 @@
-<div class="header-logo" style="margin-right: 5px;">
-	<a href="{{ route('homepage') }}">
+<div class="brand-logo">
+	<a href="{{ url('/') }}">
 		<h4 class="text-primary text-hover-secondary" style="margin-bottom: 0px;">{{ config('kobiyim.name', 'KOBIYIM') }}</h4>
 	</a>
 </div>
